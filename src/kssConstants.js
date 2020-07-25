9cb64bc928ac156c0004b796ecff84c184007b9e
Trevor F. Smith <trevor@trevor.smith.name>
2020-07-25 09:59:57 -0700

    Update to the newer postcss and prettier configs.

--- src/kssConstants.js
+++ src/kssConstants.js
@@ -1,51 +1,50 @@
-
 module.exports = {
-	somSelectors: [
-		'som',
-		'som-root',
-		'spatial-app',
-		'portal-app',
-		'immersive-app',
-		'portal-root',
-		'immersive-root',
-		'portal-som',
-		'immersive-som',
-		'som-input-group',
+    somSelectors: [
+        "som",
+        "som-root",
+        "spatial-app",
+        "portal-app",
+        "immersive-app",
+        "portal-root",
+        "immersive-root",
+        "portal-som",
+        "immersive-som",
+        "som-input-group",
 
-		'scene',	// The root of the 3D tree
-		'node',		// Any node in the 3D tree, including the root
-		'group',
-		'text',
+        "scene", // The root of the 3D tree
+        "node", // Any node in the 3D tree, including the root
+        "group",
+        "text",
 
-		'bone',
-		'line',
-		'line-loop',
-		'line-segments',
-		'lod',
-		'mesh',
-		'points',
-		'skeleton',
-		'skinned-mesh',
-		'sprite',
+        "bone",
+        "line",
+        "line-loop",
+        "line-segments",
+        "lod",
+        "mesh",
+        "points",
+        "skeleton",
+        "skinned-mesh",
+        "sprite",
 
-		'ambient-light',
-		'directional-light',
-		'hemisphere-light',
-		'light',
-		'point-light',
-		'rect-area-light',
-		'spot-light'
-	],
+        "ambient-light",
+        "directional-light",
+        "hemisphere-light",
+        "light",
+        "point-light",
+        "rect-area-light",
+        "spot-light",
+    ],
 
-	domSelectors: [
-		'body',
-		'div',
-		'document',
+    domSelectors: [
+        "body",
+        "div",
+        "document",
 
-		'dom',
-		'flat-dom',
-		'portal-dom',
-		'flat-mode',
-		'overlay-mode'
-	]
-}
+        "dom",
+        "flat-dom",
+        "portal-dom",
+        "flat-mode",
+        "overlay-mode",
+    ],
+};
